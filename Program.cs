727ab9c082ca986a443a19fa03b12f9b80ca32ab
RaquelTortelli <raquel.tortelli@gmail.com>
2024-04-09 00:19:12 -0300

    Prova resolvida

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Estagio;
+
+Prova p = new Prova();
+
+p.questao2();
+p.questao5();
---
+++ Prova.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estagio
+{
+    internal class Prova
+    {   public void questao2()
+        {
+            int numeroInformado = 21;
+            
+            int numeroAnterior = 0;
+            int numeroAtual = 1;
+            int proximoNumero;
+
+            if (numeroInformado == 0 || numeroInformado == 1)
+            {
+                Console.WriteLine("Pertence a sequência Fibonacci");
+                return;
+            }
+            
+            for (int i = 0; i <= numeroInformado; i++)
+            {
+                proximoNumero = numeroAnterior + numeroAtual;
+                numeroAnterior = numeroAtual;
+                numeroAtual = proximoNumero;
+
+                if (numeroInformado == numeroAtual)
+                {
+                    Console.WriteLine("Pertence a sequência Fibonacci");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Não pertence a sequência Fibonacci");
+        }
+
+        public void questao5()
+        {
+            string stringInformada = "Teste de reversão de string";
+            string stringInvertida = "";
+
+            for (int i = stringInformada.Length - 1; i >= 0; i--)
+            {
+                stringInvertida = stringInvertida + stringInformada[i].ToString();
+            }
+
+            Console.WriteLine(stringInvertida);
+        }
+    }
+}
